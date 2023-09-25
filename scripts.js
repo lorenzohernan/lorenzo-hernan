addb2055867d66522cb4632c8c5c55c34dc6f224
lorenzohernan <146005726+lorenzohernan@users.noreply.github.com>
2023-09-25 11:39:22 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+//alert("Hola este es mi Javascript");
+let nombre = "Martha";
+var nombre1 = "Martha";
+const nombre2 = "Martha";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+
+let contenidoTitulo = "Nombre";
+
+//let titulo = document.querySelector(".logo .fuente")
+
+//CONDICIONALES
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+if(textoTitulo== "nombre"){
+    titulo.inn
+}
